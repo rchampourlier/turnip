3fc72dc211abb3863a2a34591bd83da4fbc4fd93
Romain Champourlier <pro@rchampourlier.com>
2012-06-26 12:31:49 +0200

    Added a message to Turnip::Ambiguous error with information on ambiguous steps

--- lib/turnip/execute.rb
+++ lib/turnip/execute.rb
@@ -8,8 +8,19 @@ module Turnip
         send(method.to_s, description.to_s)
       end.compact
       raise Turnip::Pending, description if matches.length == 0
-      raise Turnip::Ambiguous, description if matches.length > 1
-      send("execute: #{matches.first.expression}", *(matches.first.params + extra_args))
+      if matches.length > 1
+        msg = "Ambiguous step definitions:\r\n"
+        matches.each_with_index do |match, index|
+          # prepare an error message with some information on the ambiguous steps
+          msg += "  #{index+1}. \"#{match.expression}\" (#{match.block.source_location.join(':')})\r\n"
+        end
+        raise Turnip::Ambiguous, msg
+      end
+      if matches.first.expression.is_a? Regexp
+        send("execute: #{matches.first.expression}", *(matches.first.params + extra_args))
+      else
+        send(matches.first.expression, *(matches.first.params + extra_args))
+      end
     end
   end
 end
